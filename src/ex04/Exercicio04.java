4f57760c7a079c0208006acd3db575ed6b14f67a
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-05-27 09:19:54 -0300

    Adicionado ex04

---
+++ src/ex04/Exercicio04.java
@@ -0,0 +1,29 @@
+package ex04;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--------------------------------");
+        System.out.println("Digite a quantidade de números: ");
+        int n = sc.nextInt();
+
+        double soma = 0;
+        for (int i = 1; i <= n; i++){
+            System.out.printf("Digite o %dª número: ", i);
+            double num = sc.nextDouble();
+
+            while (num < 0){
+                System.out.println("Entrada inválida!\nDigite um número positivo!");
+                System.out.printf("Digite o %dª número: ", i);
+                num = sc.nextDouble();
+            }
+            soma += num;
+
+        }
+        System.out.printf("A soma dos números é: %.2f", soma);
+
+
+    }
+}
