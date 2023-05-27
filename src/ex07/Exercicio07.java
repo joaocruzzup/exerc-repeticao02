efa24dd836769a1f651f5eadb774e26a264954c0
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-05-27 09:39:30 -0300

    Adicionado ex07

---
+++ src/ex07/Exercicio07.java
@@ -0,0 +1,20 @@
+package ex07;
+
+import java.util.Scanner;
+
+public class Exercicio07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número inteiro: ");
+        int num = sc.nextInt();
+
+        if ((num == 2) || (num == 3) || (num == 5)){
+            System.out.printf("O número %d é primo! %n", num);
+        } else if (((num % 2) != 0 && (num % 3) != 0 && (num % 5) != 0 )) {
+            System.out.printf("O número %d é primo! %n", num);
+        } else {
+            System.out.printf("O número %d não é primo! %n", num);
+        }
+
+    }
+}
